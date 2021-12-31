08d6433098111c952c2487db43e33673fd8343d5
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-12-31 18:09:31 +0700

    Add location model

---
+++ trackme/models/location.py
@@ -0,0 +1,28 @@
+from typing import Dict
+from trackme.models.base_model import BaseModel
+
+
+class Location(BaseModel):
+
+    def __init__(self, uid: str, timestamp: str, longitude: str, latitude: str):
+        self.uid = uid
+        self.timestamp = timestamp
+        self.longitude = longitude
+        self.latitude = latitude
+
+    def to_dict(self) -> Dict:
+        return {
+            'uid': self.uid,
+            'timestamp': self.timestamp,
+            'longitude': self.longitude,
+            'latitude': self.latitude,
+        }
+
+    @staticmethod
+    def from_dict(data: Dict) -> 'Location':
+        return Location(
+            data.get('uid'),
+            data.get('timestamp'),
+            data.get('longitude'),
+            data.get('latitude'),
+        )
