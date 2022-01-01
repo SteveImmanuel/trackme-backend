c33fcbace36328eb3a3874e798818d7050b36954
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-01-01 13:51:55 +0700

    Create get and update user endpoints

--- trackme/blueprints/__init__.py
+++ trackme/blueprints/__init__.py
@@ -1,3 +1,7 @@
 import trackme.blueprints.auth as auth
+import trackme.blueprints.bot as bot
+import trackme.blueprints.location as location
+import trackme.blueprints.info as info
+import trackme.blueprints.user as user
 
-bp_list = [auth.bp]
+bp_list = [auth.bp, bot.bp, location.bp, info.bp, user.bp]
---
+++ trackme/blueprints/user.py
@@ -0,0 +1,78 @@
+from flask import Blueprint, jsonify, make_response, g, request
+from pymongo.errors import DuplicateKeyError
+from bson.objectid import ObjectId
+from trackme.blueprints.auth import login_required
+from trackme.database.mongo.collections import Users
+from trackme.validation import UpdateUser
+from trackme.exceptions.validation_exception import ValidationException
+
+bp = Blueprint('user', __name__, url_prefix='/user')
+
+user_collection = Users()
+
+
+@bp.route('', methods=['GET'])
+@login_required
+def get():
+    try:
+        user = user_collection.find_one({'_id': ObjectId(g.get('uid'))})
+        print(ObjectId(g.get('uid')))
+        print(user)
+
+        if user is None:
+            return make_response(
+                jsonify({
+                    'code': 404,
+                    'message': 'Not Found',
+                    'detail': 'User not found'
+                }), 404)
+
+        return make_response(
+            jsonify({
+                'code': 200,
+                'message': 'Get User Successful',
+                'data': user.to_dict(),
+            }), 200)
+    except Exception as e:
+        return make_response(
+            jsonify({
+                'code': 500,
+                'message': 'Internal Server Error',
+                'detail': str(e)
+            }), 500)
+
+
+@bp.route('', methods=['PUT'])
+@login_required
+def update():
+    try:
+        data = UpdateUser.validate(request.json)
+        result = user_collection.update_one({'_id': ObjectId(g.get('uid'))}, {'$set': data})
+        print(result)
+
+        return make_response(
+            jsonify({
+                'code': 200,
+                'message': 'Update User Successful',
+                'detail': 'Status OK'
+            }), 200)
+    except ValidationException as e:
+        return make_response(jsonify({
+            'code': 400,
+            'message': 'Bad Request',
+            'detail': str(e)
+        }), 400)
+    except DuplicateKeyError as e:
+        return make_response(
+            jsonify({
+                'code': 400,
+                'message': 'Bad Request',
+                'detail': 'Username already taken'
+            }), 400)
+    except Exception as e:
+        return make_response(
+            jsonify({
+                'code': 500,
+                'message': 'Internal Server Error',
+                'detail': str(e)
+            }), 500)
