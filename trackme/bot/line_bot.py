c0e5781fb065181d8b8b45c7d77e4d50f6d7f257
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-01-02 14:48:30 +0700

    Implement manual tracking

--- trackme/bot/line_bot.py
+++ trackme/bot/line_bot.py
@@ -1,4 +1,5 @@
 import re
+import pytz
 import trackme.database.redis as redis_repository
 
 from flask import Request
@@ -11,12 +12,15 @@ from trackme.database.mongo.collections import Users
 from trackme.exceptions.bot_message_exception import BotMessageException
 from trackme.validation.add_bot_channel import AddBotChannel
 from trackme.validation.delete_bot_channel import DeleteBotChannel
+from trackme.database.influx.location_repository import LocationRepository
 from trackme.models.location import Location
+from trackme.helper.location import *
 
 # initialize connector
 api = LineBotApi(LINE_BOT_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_BOT_CHANNEL_SECRET)
 user_collection = Users()
+location_repo = LocationRepository()
 
 
 def process_webhook(request: Request):
@@ -42,6 +46,7 @@ def echo(event: MessageEvent) -> None:
     except BotMessageException as e:
         api.reply_message(event.reply_token, TextSendMessage(text=str(e)))
     except Exception as e:
+        print(e)
         api.reply_message(
             event.reply_token,
             TextSendMessage(text=str('There is trouble with the server. Please try again later')))
@@ -119,7 +124,52 @@ def unregister_channel(bot_token: str, event: MessageEvent):
 
 
 def track_location(alias: str, event: MessageEvent):
-    pass
+    alias = alias.lstrip().split(' ')
+    if len(alias) != 1:
+        raise BotMessageException('Usage: /track <alias>')
+
+    alias = alias[0]
+    source = cast(Source, event.source)
+
+    user = user_collection.find_one({
+        '$and': [
+            {
+                '$or': [
+                    {
+                        'alias': alias
+                    },
+                    {
+                        'username': alias
+                    },
+                ]
+            },
+            {
+                'bot_channels.channel_id': source.sender_id
+            },
+        ]
+    })
+
+    if user is None:
+        raise BotMessageException('Alias not found')
+
+    result = get_location_cache(user.uid)
+    if result is None:
+        data = {'uid': user.uid, 'start': '-1w'}
+        result = location_repo.find_latest_one(data)
+        result.timestamp = result.timestamp.astimezone(pytz.timezone(TIMEZONE))
+        result.timestamp = result.timestamp.strftime('%a, %d %b %I:%M %p')
+        result = result.to_dict()
+        set_location_cache(result)
+
+    if result is None:
+        raise BotMessageException(f'Location log of {alias} not found within one week')
+
+    api.reply_message(
+        event.reply_token,
+        LocationSendMessage(title=f'{alias}\'s last known location',
+                            address=result.get('timestamp'),
+                            latitude=result.get('latitude'),
+                            longitude=result.get('longitude')))
 
 
 def push_location_msg(channel_id: str, location: Location):
