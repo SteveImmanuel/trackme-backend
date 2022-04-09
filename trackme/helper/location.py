17d5f7420970043a5a4a886bceea897137f358b5
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-04-09 14:17:39 +0900

    Don't send subsequent low battery notif

--- trackme/helper/location.py
+++ trackme/helper/location.py
@@ -35,6 +35,7 @@ def get_last_location(uid: str) -> Union[None, Dict]:
             result.timestamp = result.timestamp.astimezone(pytz.timezone(TIMEZONE))
             result.timestamp = result.timestamp.strftime('%a, %d %b %I:%M %p')
             result = result.to_dict()
+            result['battery_level'] = 0
             set_location_cache(result)
             return result
         return None
@@ -44,6 +45,7 @@ def get_last_location(uid: str) -> Union[None, Dict]:
     data['longitude'] = redis_repository.hget_key(hash_key, 'longitude')
     data['latitude'] = redis_repository.hget_key(hash_key, 'latitude')
     data['timestamp'] = redis_repository.hget_key(hash_key, 'timestamp')
+    data['battery_level'] = redis_repository.hget_key(hash_key, 'timestamp')
     return data
 
 
