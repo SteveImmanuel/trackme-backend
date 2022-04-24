bf8b250feec758a1c2cb058bb7afdcfc51af24d6
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-04-24 13:20:37 +0900

    Add validation for registering account

---
+++ trackme/validation/add_connected_account.py
@@ -0,0 +1,11 @@
+from trackme.validation.base_validation import BaseValidation
+from trackme.helper.validation import *
+
+
+class AddConnectedAccount(BaseValidation):
+    validation = {
+        'id': (is_it(str), True),
+        'photo_url': (is_it(str), True),
+        'display_name': (is_it(str), True),
+        'platform': (is_it(str), True),
+    }
