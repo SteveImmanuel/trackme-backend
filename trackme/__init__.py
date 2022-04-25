5d537e5a2e8541e9f96f3c3c6bb212ad6b089d1e
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-04-25 12:10:47 +0900

    Integrate sentry for logging

--- trackme/__init__.py
+++ trackme/__init__.py
@@ -1,6 +1,15 @@
-from flask import Flask
+import sentry_sdk
 import trackme.blueprints as blueprints
+from flask import Flask
+from trackme.contants import SENTRY_DSN
+from sentry_sdk.integrations.flask import FlaskIntegration
 
+if SENTRY_DSN:
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[FlaskIntegration()],
+        traces_sample_rate=1.0
+    )
 
 def create_app():
     app = Flask(__name__)
