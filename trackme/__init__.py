4d642f67131d02673838809a36e30856addbb232
SteveImmanuel <steveandreasimanuel@gmail.com>
2023-08-11 13:16:28 +0900

    Log to sentry only in prod

--- trackme/__init__.py
+++ trackme/__init__.py
@@ -1,15 +1,13 @@
 import sentry_sdk
+import os
 import trackme.blueprints as blueprints
 from flask import Flask
 from trackme.contants import SENTRY_DSN
 from sentry_sdk.integrations.flask import FlaskIntegration
 
-if SENTRY_DSN:
-    sentry_sdk.init(
-        dsn=SENTRY_DSN,
-        integrations=[FlaskIntegration()],
-        traces_sample_rate=1.0
-    )
+if SENTRY_DSN and os.getenv('FLASK_ENV') != 'development':
+    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[FlaskIntegration()], traces_sample_rate=1.0)
+
 
 def create_app():
     app = Flask(__name__)
